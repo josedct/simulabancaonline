7ddf8aa36f1b98bec31f598b1b5510944c8d152b
eduardo carreon <ingenieroeduardoct@gmail.com>
2022-10-30 17:14:37 -0600

    Subiendo corrigiendo error en json

--- js/dcliente.js
+++ js/dcliente.js
@@ -3,7 +3,7 @@ const datosCliente = []
 
 //cargar solo los datos de los clientes que estan en el json
 async function leerDeJSON(){
-    const response = await fetch('./../json/datos.json')
+    const response = await fetch('./json/datos.json')
     const datos = await response.json()
     datos.Clientes.forEach(element => {datosCliente.push(new Cliente(element.nomCom,element.numCli,"","","",element.nipCli))})
     console.log(datos)
